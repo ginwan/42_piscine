7b17982855f28c98b197cc6c057c647d1f6fad78
Ginwan Elgasim <ginwan697@gmail.com>
2026-06-30 20:04:38 +0400

    c00, ex03 & ex04 done

---
+++ c00/ex03/ft_print_numbers.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+
+void ft_print_numbers(void)
+{
+    char num = '0';
+    while (num <= '9')
+    {
+        write(1, &num, 1);
+        num++;
+    }
+}
+
+int main(void){
+    ft_print_numbers();
+    write(1, "\n", 1);
+    return (0);
+}
