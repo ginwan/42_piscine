8b8cd217d74222fbe7c175d58360cb7095abf9b8
Ginwan Elgasim <ginwan697@gmail.com>
2026-07-18 20:35:08 +0400

    ex04 done

---
+++ c02/ex04/ft_str_is_lowercase.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_lowercase.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ginwan <ginwan@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/18 20:09:04 by ginwan            #+#    #+#             */
+/*   Updated: 2026/07/18 20:34:41 by ginwan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int ft_str_is_lowercase(char *str){
+    int i = 0;
+    while (str[i] != '\0')
+    {
+        if(str[i] >= 'a' && str[i] <= 'z'){
+            ++i;
+        } else { 
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int main(void){
+    char str[] = "abcd";
+
+    int result = ft_str_is_lowercase(str);
+    printf("%d \n", result);
+    
+    return 0;
+}
