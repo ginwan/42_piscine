e079260eed22abf1d0a983c1daa7b7e42f810323
Ginwan Elgasim <ginwan697@gmail.com>
2026-07-23 19:08:01 +0400

    ex05 done

---
+++ c03/ex05/ft_strlcat.c
@@ -0,0 +1,42 @@
+
+#include <stdio.h>
+
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int	dest_len;
+	unsigned int	src_len;
+	unsigned int	i;
+
+	dest_len = 0;
+	src_len = 0;
+	i = 0;
+
+	while (dest[dest_len])
+		dest_len++;
+
+	while (src[src_len])
+		src_len++;
+
+	if (size <= dest_len)
+		return (size + src_len);
+
+	while (src[i] && (dest_len + i) < (size - 1))
+	{
+		dest[dest_len + i] = src[i];
+		i++;
+	}
+	dest[dest_len + i] = '\0';
+
+	return (dest_len + src_len);
+}
+
+int main(void)
+{
+	char dest[20] = "Hello";
+	char src[] = " World";
+
+	printf("Return = %u\n", ft_strlcat(dest, src, 20));
+	printf("Dest   = %s\n", dest);
+
+	return (0);
+}
