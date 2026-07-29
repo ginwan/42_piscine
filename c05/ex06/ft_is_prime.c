3c49fbbb9453c9575c63502fda2604edf13396ef
Ginwan Elgasim <ginwan697@gmail.com>
2026-07-29 16:48:05 +0400

    ex06 done

---
+++ c05/ex06/ft_is_prime.c
@@ -0,0 +1,23 @@
+
+#include<stdio.h>
+
+int ft_is_prime(int nb){
+    int i;
+
+    if (nb == 0 || nb == 1)
+        return 0;
+    i = 2;
+    while(i < nb){
+        if(nb % i == 0){
+            return 0;
+        }
+        i++;
+    }
+    return 1;
+}
+
+
+int main(void){
+    printf("%d\n", ft_is_prime(13));
+    return 0;
+}
