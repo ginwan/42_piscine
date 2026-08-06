cca4251295f8f1846276d61e563b31895351c4dd
Ginwan Elgasim <ginwan697@gmail.com>
2026-08-06 11:42:39 +0400

    ex02 done

--- c08/ex01/ft_boolean.h
+++ c08/ex01/ft_boolean.h
@@ -4,11 +4,11 @@
 
 typedef int	t_bool;
 
-#define TRUE 1
-#define FALSE 0
-#define SUCCESS 0
-#define EVEN_MSG "I have an even number of arguments."
-#define ODD_MSG "I have an odd number of arguments."
+# define TRUE 1
+# define FALSE 0
+# define SUCCESS 0
+# define EVEN_MSG "I have an even number of arguments."
+# define ODD_MSG "I have an odd number of arguments."
 # define EVEN(nbr) (nbr % 2 == 0)
 
 // void ft_putstr(char *str);
